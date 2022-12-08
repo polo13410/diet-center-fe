fe5e471ae4d1b1a015952b8f0f7a722a26d4b8be
Polo13410 <polo13410@hotmail.fr>
2022-12-08 10:48:41 +0100

    Init with quasar

---
+++ src/quasar-user-options.js
@@ -0,0 +1,17 @@
+
+import './styles/quasar.sass'
+import iconSet from 'quasar/icon-set/fontawesome-v5.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+}
